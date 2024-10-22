6323d3618b73a2c47e3dabb06e4077c450992d7a
rodrigo570282 <rodrigorrs.sp@gmail.com>
2024-10-21 21:39:02 -0400

    aula 21.10

--- desenvolvimento web Thiago/HTML/projetos/show-password/main.js
+++ desenvolvimento web Thiago/HTML/projetos/show-password/main.js
@@ -3,6 +3,9 @@ console.log('hello')
 function hideShowPassword() {
     const inputPassword =
         document.getElementById('password')
+    const passwordEye=
+        document.getElementById('password-eye ')
+
 
     // se o type for password-> muda p text
     // se o type for text -> muda p password
@@ -10,10 +13,14 @@ function hideShowPassword() {
     if (inputPassword.type === 'password'){
         console.log(inputPassword.type, '-> text')
         inputPassword.type = 'text'
+
+        passwordEye.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
     } 
     
     else {
         console.log (inputPassword.type, '->password')
         inputPassword.type ='password'
+
+        passwordEye.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
     }
 }
